16d77a9f3c1650e061e9244e444866c016840643
JoyceBupt <JoyceBupt@gmail.com>
2024-05-21 22:42:21 +0800

    feat: 业务全局异常拦截器

--- admin/src/main/java/com/joycebupt/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.joycebupt.shortlink.admin.controller;
 
 import com.joycebupt.shortlink.admin.common.convention.result.Result;
+import com.joycebupt.shortlink.admin.common.convention.result.Results;
 import com.joycebupt.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.joycebupt.shortlink.admin.dto.resp.UserRespDTO;
 import com.joycebupt.shortlink.admin.service.UserService;
@@ -28,7 +29,7 @@ public class UserController {
             return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code())
                     .setMessage(UserErrorCodeEnum.USER_NULL.message());
         } else {
-            return new Result<UserRespDTO>().setCode("0").setData(result);
+            return Results.success(result);
         }
     }
 }
--- admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.joycebupt.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.joycebupt.shortlink.admin.common.convention.exception.ClientException;
+import com.joycebupt.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.joycebupt.shortlink.admin.dao.entity.UserDO;
 import com.joycebupt.shortlink.admin.dao.mapper.UserMapper;
 import com.joycebupt.shortlink.admin.dto.resp.UserRespDTO;
@@ -17,14 +19,14 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     @Override
     public UserRespDTO getUserByUsername(String username) {
-        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername, username);
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
-        UserRespDTO result = new UserRespDTO();
-        if (userDO != null) {
-            BeanUtils.copyProperties(userDO, result);
-            return result;
-        } else {
-            return null;
+        if (userDO == null) {
+            throw new ClientException(UserErrorCodeEnum.USER_NULL);
         }
+        UserRespDTO result = new UserRespDTO();
+        BeanUtils.copyProperties(userDO, result);
+        return result;
     }
 }
