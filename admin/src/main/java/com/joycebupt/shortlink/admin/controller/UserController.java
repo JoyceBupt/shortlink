ed3ea33bab0269f4b3e5eb6edf9df7d417a829e2
JoyceBupt <JoyceBupt@gmail.com>
2024-05-21 23:42:30 +0800

    feat: 检查用户名是否存在功能

--- admin/src/main/java/com/joycebupt/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/controller/UserController.java
@@ -9,6 +9,7 @@ import com.joycebupt.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,8 +29,19 @@ public class UserController {
         return Results.success(userService.getUserByUsername(username));
     }
 
+    /**
+     * 根据用户名查询无脱敏用户信息
+     */
     @GetMapping("/api/shortlink/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserInfo(@PathVariable("username") String username) {
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    /**
+     * 判断用户名是否存在
+     */
+    @GetMapping("/api/shortlink/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
     }
 }
