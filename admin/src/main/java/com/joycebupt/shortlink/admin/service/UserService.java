110ea03d113b2026bcaecac1365e917bdf62f746
JoyceBupt <JoyceBupt@gmail.com>
2024-05-22 01:14:58 +0800

    feat: 结合布隆过滤器的用户注册实现

--- admin/src/main/java/com/joycebupt/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.joycebupt.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.joycebupt.shortlink.admin.dao.entity.UserDO;
+import com.joycebupt.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.joycebupt.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -23,4 +24,11 @@ public interface UserService extends IService<UserDO> {
      * @return 用户名存在返回 True，否则返回 False
      */
     Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
--- admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.joycebupt.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,6 +8,7 @@ import com.joycebupt.shortlink.admin.common.convention.exception.ClientException
 import com.joycebupt.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.joycebupt.shortlink.admin.dao.entity.UserDO;
 import com.joycebupt.shortlink.admin.dao.mapper.UserMapper;
+import com.joycebupt.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.joycebupt.shortlink.admin.dto.resp.UserRespDTO;
 import com.joycebupt.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +39,18 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public Boolean hasUsername(String username) {
-        return userRegisterCachePenetrationBloomFilter.contains(username);
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
