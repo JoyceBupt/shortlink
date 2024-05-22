cb57a0c05c12124f9da39407acfcef77d64bf656
JoyceBupt <JoyceBupt@gmail.com>
2024-05-23 02:00:45 +0800

    feat: 查询短链接分组功能

--- admin/src/main/java/com/joycebupt/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/dao/entity/GroupDO.java
@@ -54,8 +54,8 @@ public class GroupDO extends BaseDO {
      */
     private String username;
 
-//    /**
-//     * 分组排序
-//     */
-//    private Integer sortOrder;
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
 }
--- admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.joycebupt.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.joycebupt.shortlink.admin.dao.entity.GroupDO;
 import com.joycebupt.shortlink.admin.dao.mapper.GroupMapper;
+import com.joycebupt.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.joycebupt.shortlink.admin.service.GroupService;
 import com.joycebupt.shortlink.admin.toolkit.RandomGenerator;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
@@ -24,9 +28,21 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         } while (!hasGid(gid));
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
+                .sortOrder(0)
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                // TODO 从当前上下文获取用户名
+                .eq(GroupDO::getUsername, "joyce")
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
