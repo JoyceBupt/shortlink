e4099900c3a33b52e5e0571ee64cd9dac75a0419
JoyceBupt <JoyceBupt@gmail.com>
2024-05-28 00:41:15 +0800

    feat: 注册用户后默认创建对应分组

--- admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/joycebupt/shortlink/admin/service/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import com.joycebupt.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.joycebupt.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.joycebupt.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.joycebupt.shortlink.admin.dto.resp.UserRespDTO;
+import com.joycebupt.shortlink.admin.service.GroupService;
 import com.joycebupt.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
@@ -40,6 +41,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
     private final RedissonClient redissonClient;
     private final StringRedisTemplate stringRedisTemplate;
+    private final GroupService groupService;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -76,6 +78,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                     throw new ClientException(USER_EXIST);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                groupService.saveGroup("默认分组");
                 return;
             }
             throw new ClientException(USER_NAME_EXIST);
